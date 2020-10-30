d20ff652b29ab6340bbf4e9f0ee9a9e076bfee5d
Caroline Nyakio <carolinenyakio21@gmail.com>
2020-10-30 09:19:20 +0300

    can search an image

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React,{useState, useEffect} from 'react';
 import ImageCard from './components/ImageCard';
+import ImageSearch from './components/ImageSearch';
 
 function App() {
   const [images, setImages] = useState([]);
@@ -14,15 +15,21 @@ function App() {
           setIsLoading(false);
         })
         .catch(err => console.log(err));
-  }, [] );
+  }, [term] );
 
   return (
       <div className="container mx-auto">
-        <div className="grid grid-cols-3 gap-4">
+        <ImageSearch searchText={(text) =>setTerm(text)} />
+
+        {!isLoading && images.length === 0 && <h1 className="text-4xl text-center mx-auto mt-32">
+          Image Not Found</h1> }
+
+
+        {isLoading ? <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1> : <div className="grid grid-cols-3 gap-4">
           {images.map(image => (
             <ImageCard key={image.id} image={image}/>
           ))}
-        </div>
+        </div>}
       </div>
   );
 }
